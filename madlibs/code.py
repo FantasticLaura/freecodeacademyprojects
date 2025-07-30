638aba3f62ad513c04046a924daa311f5a5bea0a
FantasticLaura <lauragazzimonti@outlook.com>
2025-07-30 14:28:23 +0200

    Create code.py
    My answer to the simple MadLibs Exercise

---
+++ madlibs/code.py
@@ -0,0 +1,6 @@
+adjective = input("Please enter an adjective: ")
+verb = input("Please enter a verb: ")
+second_verb = input("Please enter a second verb: ")
+famousPerson = input("Please enter a famour person: ")
+print(f"Computer programming is so {adjective}! It makes me so excited all the time because I love to {verb}. Stay hydrated and {second_verb} like you are {famousPerson}!")
+
